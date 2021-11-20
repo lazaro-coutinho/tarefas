747d5779b2983fe76ecbdbebeb06de4f9acb0acd
Lázaro Coutinho <lazarovasconceloscoutinho@gmail.com>
2021-11-19 23:06:11 -0300

    Listando apenas as tarefas ativas

--- src/main/java/br/com/lazaro/tarefas/repository/TarefaRepository.java
+++ src/main/java/br/com/lazaro/tarefas/repository/TarefaRepository.java
@@ -1,11 +1,17 @@
 package br.com.lazaro.tarefas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.lazaro.tarefas.model.Tarefa;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+	
+	@Query("select t from Tarefa t where t.ativo = true")
+	public List<Tarefa> findAll();
 	
 }
